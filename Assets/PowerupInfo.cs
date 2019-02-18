9eac45ffa88ac6138d09b2d7d9326ccbb1872230
Allen Eubank <adeubank@gmail.com>
2019-02-18 05:39:25 -0800

    WIP on powerup floods

---
+++ Assets/PowerupInfo.cs
@@ -0,0 +1,12 @@
+﻿public class PowerupInfo : ShapeInfo
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
