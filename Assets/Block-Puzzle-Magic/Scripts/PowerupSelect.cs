a52b2a5d30c60a9afa37f4683285920908771763
Allen Eubank <adeubank@gmail.com>
2019-05-28 06:08:45 -0700

    WIP on powerup select menu

---
+++ Assets/Block-Puzzle-Magic/Scripts/PowerupSelect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PowerupSelect : MonoBehaviour
+{
+    [SerializeField] private PowerupList availablePowerups;
+
+    public PowerupList equipedPowerups;
+
+    public GameObject powerupList;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
