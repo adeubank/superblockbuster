7aa1f6d15de88d5ce1e3a2193df42db7574d8551
Allen Eubank <adeubank@gmail.com>
2019-07-21 14:28:15 -0700

    Equip powerup after purchasing

--- Assets/Block-Puzzle-Magic/Scripts/PowerupOption.cs
+++ Assets/Block-Puzzle-Magic/Scripts/PowerupOption.cs
@@ -55,6 +55,7 @@ public class PowerupOption : MonoBehaviour
             {
                 AudioManager.Instance.PlayButtonClickSound();
                 PowerupSelectMenu.Instance.AddPurchasedPowerupId(_powerup);
+                EquipThisPowerup();
             }
             else
             {
