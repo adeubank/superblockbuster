1684f448f520d565ef631cf96657d6932cf0b0ca
Allen Eubank <adeubank@gmail.com>
2019-10-14 16:28:03 -0700

    Daily rewards are triggered at least once a day

--- Assets/DailyRewardsManager.cs
+++ Assets/DailyRewardsManager.cs
@@ -1,14 +1,77 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using UnityEngine;
 
 public class DailyRewardsManager : MonoBehaviour
 {
+    private const string DAILY_REWARDS_PREFS_KEY = "last-daily-rewards-shown";
+
+    private GameObject _dailyRewardsGameObject;
+
+    public GameObject DailyRewardsPrefab;
+
     // Start is called before the first frame update
-    private void Start()
+    private void Awake()
     {
+        StartCoroutine(ShowDailyRewards());
     }
 
-    // Update is called once per frame
-    private void Update()
+    private IEnumerator ShowDailyRewards()
     {
+        if (DailyRewardsPrefab == null)
+        {
+            Debug.LogError("Did not find DailyRewardsPrefab");
+            yield break;
+        }
+
+        Debug.Log("Checking Daily Rewards eligibility");
+
+        if (ShouldShowDailyRewards())
+        {
+            Debug.Log("Earned Daily Reward");
+            _dailyRewardsGameObject = Instantiate(DailyRewardsPrefab);
+            SetDailyRewardsCheckedAt();
+        }
+        else
+        {
+            var lastDailyRewardsCheck = LastDailyRewardsCheckedAt();
+            Debug.Log("No Daily Reward lastDailyRewardsCheck=" + lastDailyRewardsCheck.GetValueOrDefault(DateTime.Now));
+        }
+
+        yield return null;
+    }
+
+    private void SetDailyRewardsCheckedAt()
+    {
+        var now = DateTime.Now;
+        PlayerPrefs.SetString(DAILY_REWARDS_PREFS_KEY, now.ToString("o"));
+    }
+
+    private DateTime? LastDailyRewardsCheckedAt()
+    {
+        if (!PlayerPrefs.HasKey(DAILY_REWARDS_PREFS_KEY)) return null;
+
+        try
+        {
+            return DateTime.Parse(PlayerPrefs.GetString(DAILY_REWARDS_PREFS_KEY), null, DateTimeStyles.RoundtripKind);
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+
+    private bool ShouldShowDailyRewards()
+    {
+        var lastDailyRewardsCheck = LastDailyRewardsCheckedAt();
+
+        if (!lastDailyRewardsCheck.HasValue)
+        {
+            SetDailyRewardsCheckedAt();
+            return false;
+        }
+
+        return lastDailyRewardsCheck.Value.Day < DateTime.Now.Day;
     }
 }
